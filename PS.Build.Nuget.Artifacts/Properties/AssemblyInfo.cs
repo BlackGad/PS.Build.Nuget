eebae44222bc54b069d86f2118621d21c8c3318d
BlackGad <ztBlackGad@gmail.com>
2017-07-31 22:39:27 +0300

    - Implemented package artifacts project

--- PS.Build.Nuget.Artifacts/Properties/AssemblyInfo.cs
+++ PS.Build.Nuget.Artifacts/Properties/AssemblyInfo.cs
@@ -5,13 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("ClassLibrary1")]
-[assembly: AssemblyDescription("Class library description")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Handles nuget operations with PS.Build")]
 [assembly: AssemblyCompany("Vladimir Shkolka")]
-[assembly: AssemblyProduct("ClassLibrary1")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("PS.Build.Nuget.Artifacts")]
 [assembly: AssemblyCulture("en")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
